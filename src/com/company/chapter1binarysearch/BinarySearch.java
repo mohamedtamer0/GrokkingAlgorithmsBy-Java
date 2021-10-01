8203ea2bbd230aa255123e75028cd543ca3b9f0c
mohamedtamer0 <mt4452242@gmail.com>
2021-10-01 02:37:23 +0200

    SelectionSort

--- src/com/company/chapter1binarysearch/BinarySearch.java
+++ src/com/company/chapter1binarysearch/BinarySearch.java
@@ -1,7 +1,7 @@
 package com.company.chapter1binarysearch;
 
 public class BinarySearch {
-
+    // has to return boxed integer in order to comfort to interface defined in the book
     private static Integer binarySearch(int[] list, int item) {
 
         int low = 0;
@@ -9,26 +9,16 @@ public class BinarySearch {
         while (low <= high) {
             int mid = (low + high) / 2;
             int guess = list[mid];
-            if (guess == item) {
-                return mid;
-            }
-            if (guess > item) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-
-            }
+            if (guess == item) return mid;
+            if (guess > item) high = mid - 1;
+            else low = mid + 1;
         }
         return null;
     }
 
-
     public static void main(String[] args) {
         int[] myList = {1, 3, 5, 7, 9};
-
-        System.out.println(binarySearch(myList, 5));
-        System.out.println(binarySearch(myList, -1));
+        System.out.println(binarySearch(myList, 5)); // 2
+        System.out.println(binarySearch(myList, -1)); // null
     }
-
-
 }
