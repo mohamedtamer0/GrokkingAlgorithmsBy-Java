a6cf107ac42154ac28b47ca2a84885054abc886d
mohamedtamer0 <mt4452242@gmail.com>
2021-10-05 23:29:44 +0200

    BreadthFirstSearch

---
+++ src/com/company/chapter_6_Breadth_first_search/BreadthFirstSearch.java
@@ -0,0 +1,45 @@
+package com.company.chapter_6_Breadth_first_search;
+
+import java.util.*;
+
+public class BreadthFirstSearch {
+    private static final Map<String, List<String>> graph = new HashMap<>();
+
+    private static boolean search(String name) {
+        Queue<String> searchQueue = new ArrayDeque<>(graph.get(name));
+
+        List<String> searched = new ArrayList<>();
+
+        while (!searchQueue.isEmpty()) {
+            String person = searchQueue.poll();
+
+            if (!searched.contains(person))
+                if (person_is_seller(person)) System.out.println(person + "is a mango Seller!");
+                else
+                    searchQueue.addAll(graph.get(person));
+            searched.add(person);
+        }
+        return false;
+    }
+
+    private static boolean person_is_seller(String name) {
+        return name.endsWith("m");
+    }
+
+    public static void main(String[] args) {
+        graph.put("you", Arrays.asList("alice", "bob", "claire"));
+        graph.put("bob", Arrays.asList("anuj", "peggy"));
+        graph.put("alice", Arrays.asList("peggy"));
+        graph.put("claire", Arrays.asList("thom", "jonny"));
+        graph.put("anuj", Collections.emptyList());
+        graph.put("peggy", Collections.emptyList());
+        graph.put("thom", Collections.emptyList());
+        graph.put("jonny", Collections.emptyList());
+
+        search("you");
+
+
+        //Output
+        //thom is a mango seller!
+    }
+}
